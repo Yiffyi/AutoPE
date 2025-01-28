75f36040020a81c59f1a6b592c2a4f9003c3465b
Yiffyi Jia <daniel_sy@live.com>
2025-01-28 23:57:05 +0800

    add: playbook execution

--- playbook.go
+++ playbook.go
@@ -1,6 +1,17 @@
 package autope
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+
+	"github.com/yiffyi/autope/native"
+)
+
 type PlaybookPEStage struct {
+	WaitNetwork     bool `comment:"等待网络"`
+	DisableFirewall bool `comment:"关防火墙"`
+
 	DiskpartScript string `toml:",multiline" comment:"分区脚本"`
 	BootVolume     string `comment:"启动分区"`
 	SystemVolume   string `comment:"系统分区"`
@@ -16,4 +27,55 @@ type PlaybookPEStage struct {
 
 type Playbook struct {
 	PEStage PlaybookPEStage `comment:"PE阶段"`
+}
+
+func (p *PlaybookPEStage) Run() (err error) {
+
+	if is, err := IsMiniNT(); !is {
+		return fmt.Errorf("this is not suppose run here: %w", err)
+	}
+
+	var cmd *exec.Cmd
+
+	if p.WaitNetwork {
+		cmd = exec.Command("wpeutil", "WaitForNetwork")
+		err = cmd.Run()
+		if err != nil {
+			return
+		}
+	}
+
+	if p.DisableFirewall {
+		cmd = exec.Command("wpeutil", "DisableFirewall")
+		err = cmd.Run()
+		if err != nil {
+			return
+		}
+	}
+
+	if len(p.DiskpartScript) > 0 {
+		var fd *os.File
+		fd, err = os.CreateTemp("", "autope_diskpart")
+		if err != nil {
+			return err
+		}
+
+		scriptPath := fd.Name()
+		fd.WriteString(p.DiskpartScript)
+		fd.Close()
+
+		cmd = exec.Command("diskpart", "/s", scriptPath)
+		err = cmd.Run()
+		if err != nil {
+			return
+		}
+	}
+
+	if len(p.ImagePath) > 0 {
+		err = native.WIMApplyImageByPath(p.ImagePath, uint32(p.ImageIndex), p.SystemVolume)
+		if err != nil {
+			return
+		}
+	}
+	return nil
 }
